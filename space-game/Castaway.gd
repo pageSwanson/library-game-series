dfe14ca1597fcc18f52b76fdbb5b806405f7ccf8
K Swa <kpswanson@gatech.edu>
2022-02-10 20:05:13 -0500

    update session notes, push project updates
    including all session 3 code

---
+++ space-game/Castaway.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+export var speed = 200 # how fast the character moves across screen (pixels / s)
+var game_window_size
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	game_window_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2(0, 0)
+	if Input.is_action_pressed("move_down"):
+		velocity.y = velocity.y + 1
+	elif Input.is_action_pressed("move_left"):
+		velocity.x = velocity.x - 1
+	elif Input.is_action_pressed("move_up"):
+		velocity.y = velocity.x - 1
+	elif Input.is_action_pressed("move_right"):
+		velocity.x = velocity.x + 1
+
+	if velocity.length() > 0:
+		$AnimatedSprite.play("walk")
+		# if the x component of the velocity is non-zero
+		if velocity.x != 0:
+			$AnimatedSprite.flip_h = velocity.x < 0 # the sprite should flip if true - by default, this is false (don't flip)
+	else:
+		$AnimatedSprite.play("idle")
+
+	# unit-vector, scale by speed (pixels/sec), scale by factor delta (seconds since last frame was built)
+	position = position + (velocity * speed * delta)
+	position.x = clamp(position.x, 0, game_window_size.x)
+	position.y = clamp(position.y, 0, game_window_size.y)
